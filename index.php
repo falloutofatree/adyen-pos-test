15991731f2477d39bf4f1b0012618844802c69ac
Ian Garcez <ian@onespace.com.br>
2016-10-27 10:00:21 -0300

    add session for payment control

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+  session_start();
+
+  $merchantAccount   = "MerchantAccount";
+  $merchantReference = "TEST-PAYMENT-" . date("Y-m-d-H:i:s");
+  $sessid = session_id();
+
+  $_SESSION["merchantAccount"]   = $merchantAccount;
+  $_SESSION["merchantReference"] = $merchantReference;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Adyen</title>
+</head>
+<body>
+
+  <?php if (isset($_REQUEST['merchantAccount']) && isset($_REQUEST['merchantReference'])): ?>
+    <?php if (true): // check if it is approved ?>
+      <h1>Thank you for your payment</h1>
+    <?php else: ?>
+      <h1>There was a problem with your request</h1>
+    <?php endif ?>
+
+
+  <?php else: ?>
+
+    <h1>Click on the button to make a purchase</h1>
+    <button>Buy me!</button>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript">
+      (function($) {
+        var requestObject = {}
+
+        var baseUrl = "adyen://payment/?";
+
+        requestObject.sessionId = "1373375213ABCD";
+        requestObject.amount = "2000"; // = 20,00 EUR
+        requestObject.currency = "EUR";
+        requestObject.merchantReference = "MyPosOrder-123456789";
+        requestObject.callback = 'http://localhost/return.php?' + '<?php echo urlencode("sessid=" . $sessid . "&merchantAccount=" . $merchantAccount . "&merchantReference=" . $merchantReference); ?>';
+
+        function buildUrl(baseUrl, obj) {
+          var parameters = $.param(obj)
+          return baseUrl + parameters;
+        }
+
+        function launch() {
+          alert(buildUrl(baseUrl, requestObject));
+        }
+        $(function() {
+          $('button').on('click', function(e) {
+            launch();
+            e.preventDefault();
+          })
+        });
+      })(jQuery);
+    </script>
+  <?php endif ?>
+</body>
+</html>
